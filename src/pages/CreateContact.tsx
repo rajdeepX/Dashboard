c5aed6a0941946a89c1c651db4b4b066bfb9d457
Rajdeep <rajdeepdutta70@gmail.com>
2023-09-05 02:04:30 +0530

    code refactor

--- src/pages/CreateContact.tsx
+++ src/pages/CreateContact.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./CreateContact.css";
 import { addContact } from "../features/contactSlice";
 import { useDispatch } from "react-redux";
 import { nanoid } from "@reduxjs/toolkit";
