1470ec1a3cd7bcfff4f50fca13841145ca34ee1d
Rajdeep <rajdeepdutta70@gmail.com>
2023-09-05 01:45:34 +0530

    adding modal

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+// import { Link } from "react-router-dom";
 
-// import "./Contacts.css";
+import "./Nav.css";
 
 import { useDispatch } from "react-redux";
 import { deleteContact } from "../features/contactSlice";
+import Modal from "./Modal";
 
 type Contact = {
   id: string;
@@ -14,6 +15,8 @@ type Contact = {
 };
 
 const Contacts = ({ id, fName, lName, status }: Contact) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const dispatch = useDispatch();
 
   const handleDelete = (id: string) => {
@@ -22,24 +25,27 @@ const Contacts = ({ id, fName, lName, status }: Contact) => {
 
   return (
     <div className="flex flex-col gap-4 bg-[#ddd] p-[7px] rounded-[7px]">
-      <div className="flex flex-col gap-3 p-3">
-        <h2 className="capitalize text-xl">{fName}</h2>
-        <h2 className="capitalize text-xl">{lName}</h2>
-        <h2 className="font-medium">{status}</h2>
+      <div className="p-3">
+        <h2 className="capitalize text-xl text-center">{fName}</h2>
+        {/* <h2 className="capitalize text-xl">{lName}</h2> */}
+        {/* <h2 className="font-medium">{status}</h2> */}
       </div>
-      <div className="flex justify-between">
-        <Link to={`/edit-contact/${id}`}>
-          <button className="bg-[rgb(126,189,33)] text-[black] px-[15px] py-[7px] rounded-[7px]">
-            Edit
-          </button>
-        </Link>
-        <button
-          className="bg-[rgb(230,24,24)] text-white px-[15px] py-[7px] rounded-[7px]"
-          onClick={() => handleDelete(id)}
-        >
-          Delete
-        </button>
-      </div>
+      <button
+        onClick={() => setIsOpen(true)}
+        className="px-[12px] py-[7px] bg-[#808080] text-white rounded-[7px]"
+      >
+        View Details
+      </button>
+
+      <Modal
+        fName={fName}
+        lName={lName}
+        status={status}
+        handleDelete={handleDelete}
+        id={id}
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
     </div>
   );
 };
---
+++ src/components/Modal.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+const Modal = ({
+  fName,
+  lName,
+  status,
+  id,
+  handleDelete,
+  open,
+  onClose,
+}: any) => {
+  if (!open) return null;
+
+  return (
+    <>
+      <div className="fixed bg-[rgba(0,0,0,0.7)] z-[100] inset-0"></div>
+      <div className="flex flex-col gap-4 p-[7px] rounded-[7px] fixed -translate-x-2/4 -translate-y-2/4 bg-white z-[1000] w-[200px] left-2/4 top-2/4">
+        <div className="flex flex-col gap-3 p-3 text-center">
+          <h2 className="capitalize text-xl">{fName}</h2>
+          <h2 className="capitalize text-xl">{lName}</h2>
+          <h2 className="font-medium">{status}</h2>
+        </div>
+        <div className="flex justify-around">
+          <Link to={`/edit-contact/${id}`}>
+            <button className="bg-[rgb(126,189,33)] text-[black] px-[15px] py-[7px] rounded-[7px]">
+              Edit
+            </button>
+          </Link>
+          <button
+            className="bg-[rgb(230,24,24)] text-white px-[15px] py-[7px] rounded-[7px]"
+            onClick={() => handleDelete(id)}
+          >
+            Delete
+          </button>
+          <button
+            className="absolute right-3 top-[7px] text-indigo-500"
+            onClick={onClose}
+          >
+            X
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
