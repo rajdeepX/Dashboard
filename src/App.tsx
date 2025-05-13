d9ab6bd4d45d276fe57875ec360385f751e69688
Rajdeep Dutta <rajdeepdutta70@gmail.com>
2025-05-13 23:35:25 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
-import ContactPage from "./pages/ContactPage";
-import Data from "./components/Data";
-import CreateContact from "./pages/CreateContact";
-import EditContact from "./pages/EditContact";
 import Charts from "./pages/Charts";
-import Map from "./components/Map";
 
 function App() {
   return (
